d61b0bd10b44ea4c3482185b2cb243f0bf27b44e
Thalles Rodrigues <thalles.cezar@gmail.com>
2019-01-16 21:12:08 -0300

    Reuse mutant instance

--- src/leaflet.google.js
+++ src/leaflet.google.js
@@ -98,8 +98,8 @@ L.GridLayer.GoogleMutant = L.GridLayer.extend({
   
   onRemove(map) {
     L.GridLayer.prototype.onRemove.call(this, map);
+    this._observer.disconnect();
     map._container.removeChild(this._mutantContainer);
-    this._mutantContainer = undefined;
     
     map.off("viewreset", this._reset, this);
     map.off("move", this._update, this);
@@ -137,8 +137,8 @@ L.GridLayer.GoogleMutant = L.GridLayer.extend({
       // 			this._mutantContainer.style.zIndex = 'auto';
       this._mutantContainer.style.pointerEvents = "none";
       
-      this._map.getContainer().appendChild(this._mutantContainer);
     }
+    this._map.getContainer().appendChild(this._mutantContainer);
     
     this.setOpacity(this.options.opacity);
     this.setElementSize(this._mutantContainer, this._map.getSize());
@@ -148,6 +148,13 @@ L.GridLayer.GoogleMutant = L.GridLayer.extend({
   
   _initMutant() {
     if ( !this._ready || !this._mutantContainer ) return;
+
+    if (this._mutant) {
+			// reuse old _mutant, just make sure it has the correct size
+			this._resize();
+			return;
+		}
+
     this._mutantCenter = new google.maps.LatLng(0, 0);
     
     let map = new google.maps.Map(this._mutantContainer, {
@@ -176,14 +183,20 @@ L.GridLayer.GoogleMutant = L.GridLayer.extend({
   
   _attachObserver: function _attachObserver(node) {
     // 		console.log('Gonna observe', node);
-    
-    let observer = new MutationObserver(this._onMutations.bind(this));
+
+    if (!this._observer)
+			this._observer = new MutationObserver(this._onMutations.bind(this));
     
     // pass in the target node, as well as the observer options
-    observer.observe(node, {
-      childList: true,
-      subtree: true,
-    });
+    this._observer.observe(node, { childList: true, subtree: true });
+
+		// if we are reusing an old _mutantContainer, we must manually detect
+		// all existing tiles in it
+		Array.prototype.forEach.call(
+			node.querySelectorAll('img'),
+			this._boundOnMutatedImage
+		);
+
   },
   
   _onMutations: function _onMutations(mutations) {
